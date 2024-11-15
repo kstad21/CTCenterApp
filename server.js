d15031f7dfaa403e63fae5b4f9e922add882134c
Katy Stadler <100023534@mvla.net>
2024-11-14 23:34:45 -0800

    just some notes on future goals

--- server.js
+++ server.js
@@ -22,3 +22,15 @@ mongoose.connect(uri).then(() => console.log("Connected to MongoDB"))
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+/*
+Notes: 
+- make the list of tutors clickable (instead of just searching) and then JUST show that
+  one tutors info 
+- MODIFY a tutor; and what if i try to add a tutor that's already there? same w/ courses.
+- display <today's> appts on the front page, add appt, search appt, appt schema, appt endpoint
+- ^ in this vein, make sure that tutors can have arrays of the appt schemas? and the course 
+  ones too maybe? 
+- do i keep track of students or is that too sensitive (like there's hr stuff)? could i just
+  keep track of id's or smth? bc when u make an appt we wanna be able to mark/id a student down
+*/
